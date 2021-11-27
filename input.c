00a7c25f1691e2ff059a9cf2a8437cb1b5cb933f
Jiyoun Kim <0715jiyounkim@gmail.com>
2021-11-27 19:56:46 +0900

    feat: lexical analyser with grammar rules

---
+++ input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+	int x;
+	char a;
+	int y;
+	int z;
+	x=3;
+	a='r';
+	y=10;
+	z=9;
+	if(x<y){
+		if(z<y){
+			x=50;
+			y=3+9*2;
+		}
+	}
+	a='i'
+	return 0;
+}
+	
